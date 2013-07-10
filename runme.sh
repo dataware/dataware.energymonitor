2444ac12b6a318f24ecdcdefe2f52a8e3e0e397e
root <root@sheevaplug-debian.(none)>
2013-07-10 02:03:44 -0400

    small changes

--- runme.sh
+++ runme.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-cd bin
-java -classpath .:../libs/RXTXcomm.jar:../libs/jsrpc.jar uk/ac/nott/cs/txl/energy/Monitor /dev/ttyUSB0
+cd /root/dataware.energymonitor/bin
+nohup java -classpath .:../libs/RXTXcomm.jar:../libs/jsrpc.jar uk/ac/nott/cs/txl/energy/Monitor $1 & 
+echo -n $! " " >> /var/run/energymonitor.pid
